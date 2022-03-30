b271e89d4cd3a78a7524e423d243e2ba5b6306f5
ankita658 <81344818+ankita658@users.noreply.github.com>
2022-03-30 12:45:06 +0530

    Add files via upload

---
+++ 3_Implementation/tictac_operations.h
@@ -0,0 +1,11 @@
+  #ifndef PASSWIN_H
+  #define PASSWIN_H
+  
+  int posswin(int p);
+  void draw_board();
+  void help();
+  void start_game();
+  void check_draw();
+  void put_X_O(char ch,int pos);
+  
+  #endif
