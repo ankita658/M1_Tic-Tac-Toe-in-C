069896b782d969089b94c9c6548cba6242f305f7
ankita658 <81344818+ankita658@users.noreply.github.com>
2022-03-31 13:03:53 +0530

    Create check_draw.c

---
+++ 3_Implementation/src/check_draw.c
@@ -0,0 +1,15 @@
+#include "game_operations.h"
+
+int check_draw(int turn)
+{
+    if(turn > 9)
+    {
+        //gotoxy(30,20);
+        //textcolor(128+RED);
+        printf("\n\n Game Draw");
+        getch();
+        exit(0);
+    }
+    return 1;
+}
+© 2022 GitHub, Inc.
